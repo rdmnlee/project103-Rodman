3a0c7b99a0aca7d8b5884c4fa74c7556940ca687
rdmnlee <rodmanperezlee@gmail.com>
2021-03-15 23:00:41 +0800

    Add calendar

--- index.php
+++ index.php
@@ -17,20 +17,20 @@
   <body>
     
 <nav class="navbar navbar-expand-md navbar-dark fixed-top bg-dark">
-  <a class="navbar-brand" href="#">Fixed navbar</a>
+  <a class="navbar-brand" href="#">My page</a>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
   </button>
   <div class="collapse navbar-collapse" id="navbarCollapse">
     <ul class="navbar-nav mr-auto">
       <li class="nav-item active">
-        <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+        <a class="nav-link" href="main.php">Home <span class="sr-only">(current)</span></a>
       </li>
       <li class="nav-item">
         <a class="nav-link" href="/pages/calculator.php">Calculator</a>
       </li>
       <li class="nav-item">
-        <a class="nav-link" href="#">Calendar</a>
+        <a class="nav-link" href="/pages/index.php">Calendar</a>
       </li>
     </ul>
     <form class="form-inline mt-2 mt-md-0">
@@ -50,7 +50,7 @@
   <div class="jumbotron">
     <h1>Calendar</h1>
     <p class="lead">This is the calendar application.</p>
-    <a class="btn btn-lg btn-primary" href="/pages/calculator.php" role="button">Go to Calendar &raquo;</a>
+    <a class="btn btn-lg btn-primary" href="/pages/index.php" role="button">Go to Calendar &raquo;</a>
   </div>
 </main>
 
--- pages/calculator.php
+++ pages/calculator.php
@@ -1,54 +1,64 @@
 <!DOCTYPE html>
+<html>
+	<head>
+		<title>Calculator App</title>
+		<meta charset="utf-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		 
+		<link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css" rel="stylesheet">
+	</head>
+	<body>
+    <a class="nav-link" style="font-size:30px;" href="/">Go back</a>
+		<div class="container" style="margin-top: 80px; display: flex; justify-content: center;" >
+        
+			<?php
+				if(isset($_POST['submit']))
+				{
 
-<html lang="en">
-<head>
-    <title>Calculator</title>
+					if(is_numeric($_POST['number1']) && is_numeric($_POST['number2']))
+					{
 
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+						if($_POST['operation'] == 'plus')
+						{
+							$total = $_POST['number1'] + $_POST['number2'];	
+						}
+						if($_POST['operation'] == 'minus')
+						{
+							$total = $_POST['number1'] - $_POST['number2'];	
+						}
+						if($_POST['operation'] == 'multiply')
+						{
+							$total = $_POST['number1'] * $_POST['number2'];	
+						}
+						if($_POST['operation'] == 'divided by')
+						{
+							$total = $_POST['number1'] / $_POST['number2'];	
+						}
+						
+						echo "Answer: {$total}";
+					
+					} else {
+						
+						echo 'Numeric values are required';
+					
+					}
+				}
+			?>
+		</div>
+		    <form method="post" action="calculator.php" style="display: flex; justify-content: center;">
+		        <input name="number1" type="text" class="form-control" style="width: 150px; display: inline" />
+		        <select name="operation">
+		        	<option value="plus">Plus</option>
+		            <option value="minus">Minus</option>
+		            <option value="multiply">Multiply</option>
+		            <option value="divided by">Divide</option>
+		        </select>
+		        <input name="number2" type="text" class="form-control" style="width: 150px; display: inline" />
+		        <input name="submit" type="submit" value="Calculate" class="btn btn-primary" />
+		    </form>
+	    
 
-    <link href="https://fonts.googleapis.com/css?family=Roboto:300" rel="stylesheet">
-    <link href="/assets/css/calculator.css" rel="stylesheet">
-</head>
-
-<body>
-    <header>
-        <h1>Calculator</h1>
-    </header>
-
-    <main>
-        <div id="container">
-            <div class="row clearfix">
-                <input id="screen" value="0" disabled type="text">
-                <button id="clear">AC</button>
-            </div>
-            <div class="row clearfix">
-                <button class="number">1</button>
-                <button class="number">2</button>
-                <button class="number">3</button>
-                <button data-operator="+" class="operator">+</button>
-            </div>
-            <div class="row clearfix">
-                <button class="number">4</button>
-                <button class="number">5</button>
-                <button class="number">6</button>
-                <button data-operator="-" class="operator">-</button>
-            </div>
-            <div class="row clearfix">
-                <button class="number">7</button>
-                <button class="number">8</button>
-                <button class="number">9</button>
-                <button data-operator="*" class="operator">×</button>
-            </div>
-            <div class="row clearfix">
-                <button id="decimal">.</button>
-                <button class="number">0</button>
-                <button id="equals">=</button>
-                <button data-operator="/" class="operator">÷</button>
-            </div>
-        </div>
-    </main>
-
-    <script src="/assets/js/calculator.js"></script>
-</body>
+	
+	</body>
 </html>
